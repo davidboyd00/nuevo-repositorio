bc39240924bef3c67fcd0a42ce7073af9d885549
davidboyd00 <david.boyd@uc.cl>
2024-09-12 15:28:06 -0300

    url imagen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import foodImage from "../assets/images/DCC.png";  // Importa la imagen
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                <img src={foodImage} alt="DCChef Food" className="logo" /> {/* Usa la imagen importada */}
+                <img src="https://staticnew-prod.topdoctors.cl/article/6591/image/large/celebremos-el-18-de-septiembre-1599498597.jpg" alt="DCChef Food" className="logo" /> {/* Usa el URL de la imagen */}
                 <span className="navbar-title">DCChef</span>
             </div>
             <div className="navbar-links">
